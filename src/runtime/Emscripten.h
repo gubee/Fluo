3d99473a1c45313c545cb673a71cf9a5515b8469
gubee <gubee@hotmail.com>
2013-06-21 17:35:18 +0900

    Coding Guideline Applied

--- src/runtime/Emscripten.h
+++ src/runtime/Emscripten.h
@@ -13,22 +13,22 @@
 //----------------------------------------------------------------------------------------------
 // StackFrame APIs
 extern "C" {
-StackFrame* StackFrame_new();
-void StackFrame_delete(StackFrame* stackFrame);
+    StackFrame* StackFrame_new();
+    void StackFrame_delete(StackFrame* stackFrame);
 }
 
 //----------------------------------------------------------------------------------------------
 // List APIs
 extern "C" {
-List* List_new(Type type);
-void List_delete(List* list);
-void List_append(List* list, Value value);
-void List_remove(List* list, Value value, bool removeAll);
-void List_removeAt(List* list, int index);
-int List_count(List* list);
-int List_indexOf(List* list, Value value);
-Value List_at(List* list, int index);
-void List_setAt(List* list, int index, Value value);
+    List* List_new(Type type);
+    void List_delete(List* list);
+    void List_append(List* list, Value value);
+    void List_remove(List* list, Value value, bool removeAll);
+    void List_removeAt(List* list, int index);
+    int List_count(List* list);
+    int List_indexOf(List* list, Value value);
+    Value List_at(List* list, int index);
+    void List_setAt(List* list, int index, Value value);
 }
 
 //----------------------------------------------------------------------------------------------
