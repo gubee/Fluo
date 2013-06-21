57a3b0d9f0f92f91c09a2d40ef04be46ac116670
gubee <gubee@hotmail.com>
2013-06-21 14:42:18 +0900

    Initial Project Files

---
+++ src/Fluo.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Fluo.cpp
+// Author      : Fluo Team
+// Version     :
+// Copyright   : Fluo, All Rights Reserved
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
