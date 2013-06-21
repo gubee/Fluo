5dc201aab55473a28d5236ae173b192f44e2ac7f
gubee <gubee@hotmail.com>
2013-06-21 17:20:56 +0900

    Coding Guideline Applied

--- src/kernel/Fluorine.h
+++ src/kernel/Fluorine.h
@@ -8,48 +8,44 @@
 #ifndef FLUORINE_H_
 #define FLUORINE_H_
 
-
 //----------------------------------------------------------------------------------------------
 // Type
 enum Type {
-	Bool,
-	Int,
-	Real,
-	String,
-	Point,
-	Size,
-	Rect,
-	List,
-	Map,
-	ScriptObject,
-	Object
+    Bool,
+    Int,
+    Real,
+    String,
+    Point,
+    Size,
+    Rect,
+    List,
+    Map,
+    ScriptObject,
+    Object
 };
 
-typedef void*		Value;
-typedef int*		Address;
-
+typedef void* Value;
+typedef int* Address;
 
 //----------------------------------------------------------------------------------------------
 // StackFrame
 struct StackFrame {
-	StackFrame*		previousFrame;
-	Address			framePointer;
-	int				argumentCount;
-	int				arguments[6];		// framePointer offsets
+    StackFrame* previousFrame;
+    Address framePointer;
+    int argumentCount;
+    int arguments[6];		// framePointer offsets
 };
-
 
 //----------------------------------------------------------------------------------------------
 // List
 struct List {
-	Type	type;
+    Type type;
 };
-
 
 //----------------------------------------------------------------------------------------------
 // Map
 struct Map {
-	Type	type;
+    Type type;
 };
 
 #endif /* FLUORINE_H_ */
