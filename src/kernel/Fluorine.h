6f220f0967d5adcdf67ecdee4996b300f8e6dc6f
gubee <gubee@hotmail.com>
2013-06-21 16:47:11 +0900

    StackFrame, List APIs Definition

---
+++ src/kernel/Fluorine.h
@@ -0,0 +1,56 @@
+/*
+ * Fluorine.h
+ *
+ *  Created on: 2013. 6. 21.
+ *      Author: gubee
+ */
+
+#ifndef FLUORINE_H_
+#define FLUORINE_H_
+
+
+//----------------------------------------------------------------------------------------------
+// Type
+enum Type {
+	Bool,
+	Int,
+	Real,
+	String,
+	Point,
+	Size,
+	Rect,
+	List,
+	Map,
+	ScriptObject,
+	Object
+};
+
+typedef void*		Value;
+typedef int*		Address;
+
+
+//----------------------------------------------------------------------------------------------
+// StackFrame
+struct StackFrame {
+	StackFrame*		previousFrame;
+	Address			framePointer;
+	int				argumentCount;
+	int				arguments[6];		// framePointer offsets
+};
+
+
+//----------------------------------------------------------------------------------------------
+// List
+struct List {
+	Type	type;
+};
+
+
+//----------------------------------------------------------------------------------------------
+// Map
+struct Map {
+	Type	type;
+};
+
+
+#endif /* FLUORINE_H_ */
